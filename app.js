85c1480a37df675f852db2a147104e9e1faa1d8f
George Thomas <george@thomas.name>
2012-08-06 17:49:34 -0400

    package.json rocks

--- app.js
+++ app.js
@@ -6,8 +6,8 @@
 var express = require('express')
   , routes = require('./routes');
 
-var app = module.exports = express();
-//var app = module.exports = express.createServer();
+//var app = module.exports = express();
+var app = module.exports = express.createServer();
 
 // Configuration
 
@@ -38,6 +38,6 @@ app.get('/about', routes.about);
 app.get('/work', routes.work); //wow i'm good...
 
 app.listen(3000, function(){
-//  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
-  console.log("Express server listening on port 3000");
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+// console.log("Express server listening on port 3000");
 });
