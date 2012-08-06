b898473e9a67eb14c1c268ca36954fb3426663b7
George Thomas <george@thomas.name>
2012-08-06 17:36:29 -0400

    app.address().port

--- app.js
+++ app.js
@@ -38,5 +38,6 @@ app.get('/about', routes.about);
 app.get('/work', routes.work); //wow i'm good...
 
 app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+//  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", app.address.port, app.settings.env);
 });
